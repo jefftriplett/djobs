9b636a15b4e8ac8f0eda32902cb277e432792f36
Jeff Triplett <jeff.triplett@gmail.com>
2020-01-10 23:56:09 -0600

    :pencil: Adds job fixtures

---
+++ jobs/tests/fixtures.py
@@ -0,0 +1,13 @@
+import pytest
+
+from model_bakery import baker
+
+
+@pytest.fixture()
+def flag(db, joblisting):
+    return baker.make("jobs.Flag", job=joblisting)
+
+
+@pytest.fixture()
+def joblisting(db):
+    return baker.make("jobs.JobListing")
