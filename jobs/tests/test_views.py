f84eb58a32776cf54913b79c3acf37affda98d41
Jeff Triplett <jeff.triplett@gmail.com>
2020-01-11 11:17:35 -0600

    :green_heart: MVP tests

--- jobs/tests/test_views.py
+++ jobs/tests/test_views.py
@@ -2,6 +2,20 @@ import pytest
 
 
 @pytest.mark.django_db
+def test_job_archive_get(tp, django_assert_num_queries, joblisting):
+    with django_assert_num_queries(0):
+        response = tp.get("job_archive", pk=joblisting.pk)
+    tp.assert_http_302_found(response)
+
+
+@pytest.mark.django_db
+def test_job_create_get(tp, django_assert_num_queries):
+    with django_assert_num_queries(0):
+        response = tp.get("job_create")
+    tp.assert_http_302_found(response)
+
+
+@pytest.mark.django_db
 def test_job_detail_get(tp, django_assert_num_queries, joblisting):
     with django_assert_num_queries(3):
         response = tp.get("job_detail", pk=joblisting.pk)
