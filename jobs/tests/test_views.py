e698537946aacd5545d619cbbaa75180c765e0a3
Jeff Triplett <jeff.triplett@gmail.com>
2020-01-11 11:55:23 -0600

    :green_heart: More complete view tests

--- jobs/tests/test_views.py
+++ jobs/tests/test_views.py
@@ -23,6 +23,13 @@ def test_job_detail_get(tp, django_assert_num_queries, joblisting):
 
 
 @pytest.mark.django_db
+def test_job_edit_get(tp, django_assert_num_queries, joblisting):
+    with django_assert_num_queries(0):
+        response = tp.get("job_edit", pk=joblisting.pk)
+    tp.assert_http_302_found(response)
+
+
+@pytest.mark.django_db
 def test_job_feed_get(tp, django_assert_num_queries):
     with django_assert_num_queries(1):
         response = tp.get("job_feed")
@@ -30,7 +37,42 @@ def test_job_feed_get(tp, django_assert_num_queries):
 
 
 @pytest.mark.django_db
+def test_job_flag_get(tp, django_assert_num_queries, joblisting):
+    with django_assert_num_queries(0):
+        response = tp.get("job_flag", pk=joblisting.pk)
+    tp.assert_http_405_method_not_allowed(response)
+
+
+@pytest.mark.django_db
+def test_job_publish_get(tp, django_assert_num_queries, joblisting):
+    with django_assert_num_queries(0):
+        response = tp.get("job_publish", pk=joblisting.pk)
+    tp.assert_http_302_found(response)
+
+
+@pytest.mark.django_db
 def test_job_list_get(tp, django_assert_num_queries):
     with django_assert_num_queries(1):
         response = tp.get("job_list")
     tp.assert_http_200_ok(response)
+
+
+@pytest.mark.django_db
+def test_login_get(tp, django_assert_num_queries):
+    with django_assert_num_queries(0):
+        response = tp.get("login")
+    tp.assert_http_404_not_found(response)
+
+
+@pytest.mark.django_db
+def test_logout_get(tp, django_assert_num_queries):
+    with django_assert_num_queries(0):
+        response = tp.get("logout")
+    tp.assert_http_302_found(response)
+
+
+@pytest.mark.django_db
+def test_review_flags_get(tp, django_assert_num_queries):
+    with django_assert_num_queries(0):
+        response = tp.get("flags")
+    tp.assert_http_404_not_found(response)
