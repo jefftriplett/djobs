c4892f4c4c0f443c849bebac54f83221a691dd3e
Jeff Triplett <jeff.triplett@gmail.com>
2020-01-10 22:26:43 -0600

    :pencil: Hide mailhide for now

--- jobs/templatetags/mailhide.py
+++ jobs/templatetags/mailhide.py
@@ -1,17 +1,17 @@
-import recaptcha.client.mailhide
-from django import template
-from django.conf import settings
+# import recaptcha.client.mailhide
+# from django import template
+# from django.conf import settings
 
-register = template.Library()
+# register = template.Library()
 
 
-@register.filter
-def mailhide(addr):
-    if (
-        settings.MAILHIDE_PUBLIC_KEY and settings.MAILHIDE_PRIVATE_KEY
-    ):  # pragma: no cover
-        return recaptcha.client.mailhide.asurl(
-            addr, settings.MAILHIDE_PUBLIC_KEY, settings.MAILHIDE_PRIVATE_KEY
-        )
-    else:
-        return addr
+# @register.filter
+# def mailhide(addr):
+#     if (
+#         settings.MAILHIDE_PUBLIC_KEY and settings.MAILHIDE_PRIVATE_KEY
+#     ):  # pragma: no cover
+#         return recaptcha.client.mailhide.asurl(
+#             addr, settings.MAILHIDE_PUBLIC_KEY, settings.MAILHIDE_PRIVATE_KEY
+#         )
+#     else:
+#         return addr
