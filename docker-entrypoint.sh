c83ec0d99d02a3d5aff13b4a4c53bfdd05ab67f1
Jeff Triplett <jeff.triplett@gmail.com>
2020-08-10 23:45:58 -0500

    :tractor: Some move bits around

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+# python manage.py migrate --noinput
+# python manage.py collectstatic --noinput
 
 exec "$@"
