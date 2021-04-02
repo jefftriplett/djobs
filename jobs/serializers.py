8865abc7e18dbbe3f4e2a7541f36cbe0de5dfcf1
Katherine Michel <kthrnmichel@gmail.com>
2021-04-02 12:05:13 -0500

    :hammer: Fixing formatting

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -3,9 +3,22 @@ from .models import JobListing
 
 
 class JobListingSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = JobListing
-        fields = ('creator', 'created', 'title', 'description', 'compensation', 'location', 'location_latitude', 'location_longitude', 'skills', 'remote', 'employer_name', 'employer_website', 'contact_name', 'contact_email')
+        fields = [
+            'creator', 
+            'created', 
+            'title', 
+            'description', 
+            'compensation', 
+            'location', 
+            'location_latitude', 
+            'location_longitude', 
+            'skills', 'remote', 
+            'employer_name', 
+            'employer_website', 
+            'contact_name', 
+            'contact_email'
+            ]
 
 
